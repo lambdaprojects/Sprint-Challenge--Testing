5c7a1f0db419b866485e4c85578492175b886a30
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-21 10:31:49 -0500

    SC:Testing - Server.js Testing complete
    Completed the testing for server.js

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,23 @@
 const request = require("supertest");
+const server = require("./server");
 
 describe("TS1: SERVER TESTING", () => {
   it("TC1: SERVER ENV = TESTING", () => {
     expect(process.env.DB_ENV).toBe("testing");
+  });
+
+  it("TC2: GET REQUEST STATUS 200", async () => {
+    const res = await request(server).get("/");
+    expect(res.status).toBe(200);
+  });
+
+  it("TC3: GET RESPONSE TYPE - APPLICATION/JSON", async () => {
+    const res = await request(server).get("/");
+    expect(res.type).toBe("application/json");
+  });
+
+  it("TC4: GET RESPONSE MESSAGE - APPLICATION/JSON", async () => {
+    const res = await request(server).get("/");
+    expect(res.body.message).toBe("The api is up!");
   });
 });
